cf49220700ee6244e8ef3270107f9861b8736ded
Victor <VictorAlexSBr@gmail.com>
2025-01-04 17:29:54 -0300

    Lista 3, Exercício 06 Completo

--- Lista 3/06-enigma.py
+++ Lista 3/06-enigma.py
@@ -1,32 +1,15 @@
-#cifrada = input()
-#crib = input()
+cifrada = input()
+crib = input()
 
-cifrada = "FDMLCRDMRALF"
-crib = ['A','R','M','A','D','A']
 possiveis = 0
 
-print(cifrada)
-for i in range(len(crib)):
+for i in range(len(cifrada)-len(crib)+1):
     naoDa = False
-    for j in range(len(crib)):
-        print(crib)
-        if cifrada[j] == crib[j]:
+    for j in range(len(crib)): 
+        if cifrada[i+j] == crib[j]: 
             naoDa = True
-            break;
-        else:
-            print(cifrada[j],'!=',crib[j])
-            print("+1")
-            possiveis += 1;
+            break
         
-    if i < 8: crib.insert(0,'0')
-        
+    if not naoDa: possiveis += 1
+
 print(possiveis)
-
-
-#FDMLCRDMRALF
-#ARMADA
-#0ARMADA
-#00ARMADA
-#000ARMADA
-#0000ARMADA
-#00000ARMADA
